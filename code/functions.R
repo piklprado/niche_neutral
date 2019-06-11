b0dcf7599a9b524a583ae95db2a3d7ffdec01d6e
piklprado <paulo@PI.localdomain>
2019-06-10 22:37:36 -0300

    New plot of metacommunity rank-abundance

--- code/functions.R
+++ code/functions.R
@@ -1,3 +1,8 @@
+library(grid)
+library(ggthemes)
+library(scales)
+library(gridExtra)
+
 ################################################################################
 ## Functions to fit the competing models to abundance data
 ################################################################################
@@ -288,3 +293,53 @@ r2.neutral <- function(model, null.model, ...){
     return(list(full.table=r2.tab, part.table=r2.partition, R2=R2))
 }
 
+################################################################################
+## GGplot theme
+################################################################################
+##### Theming plots for publication ##
+## https://rpubs.com/Koundy/71792
+
+theme_Publication <- function(base_size=14, base_family="helvetica") {
+      (theme_foundation(base_size=base_size, base_family=base_family)
+       + theme(plot.title = element_text(size = rel(1), hjust = 0.5),
+               text = element_text(),
+               panel.background = element_rect(colour = NA),
+               plot.background = element_rect(colour = NA),
+               panel.border = element_rect(colour = NA),
+               axis.title = element_text(face = "bold",size = rel(1)),
+               axis.title.y = element_text(angle=90,vjust =2),
+               axis.title.x = element_text(vjust = -0.2),
+               axis.text = element_text(), 
+               axis.line = element_line(colour="black"),
+               axis.ticks = element_line(),
+               panel.grid.major = element_line(colour="#f0f0f0"),
+               panel.grid.minor = element_blank(),
+               legend.key = element_rect(colour = NA),
+               legend.position = c(0.9, 0.8),
+               legend.direction = "vertical",
+               legend.key.size= unit(0.1, "cm"),
+               legend.spacing = unit(0, "cm"),
+               ##legend.title = element_text(face="italic"),
+               legend.text = element_text(size=rel(0.75)),
+               legend.title = element_text(face="bold", size=rel(0.75)),
+               plot.margin=unit(c(10,5,5,5),"mm"),
+               strip.background=element_rect(colour="#f0f0f0",fill="#f0f0f0"),
+               strip.text = element_text(face="bold")
+          ))      
+}
+
+scale_fill_Publication <- function(...){
+    discrete_scale("fill","Publication",
+                   manual_pal(values = c("#386cb0","#fdb462",
+                                         "#7fc97f","#ef3b2c",
+                                         "#662506","#a6cee3",
+                                         "#fb9a99","#984ea3","#ffff33")), ...)
+}
+
+scale_colour_Publication <- function(...){
+      discrete_scale("colour","Publication",
+                     manual_pal(values = c("#386cb0","#fdb462",
+                                           "#7fc97f","#ef3b2c",
+                                           "#662506","#a6cee3",
+                                           "#fb9a99","#984ea3","#ffff33")), ...)
+}
