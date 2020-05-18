40a9962a8f7df1bce22f1b51f743a186a7aa0463
saramortara <saramortara@gmail.com>
2020-05-18 14:55:22 -0300

    code styling

--- code/02_modeling.R
+++ code/02_modeling.R
@@ -4,7 +4,7 @@
 #####################################################################################
 
 ## Code for applying the niche-neutral GLMM framework
-## Data and analysis from the manuscript  
+## Data and analysis from the manuscript
 
 ###############################
 # PART 1: loading packages ####
@@ -18,7 +18,7 @@ library(piecewiseSEM)
 library(dplyr)
 library(ggplot2)
 library(sads)
-# functions to calculate R2 
+# functions to calculate R2
 source("functions.R")
 
 ############################
@@ -26,20 +26,18 @@ source("functions.R")
 ############################
 
 ## abundant
-fern.data.ab <- read.csv("../data/data_ab_for_modeling.csv", as.is=TRUE)
+fern.data.ab <- read.csv("../data/data_ab_for_modeling.csv", as.is = TRUE)
 
 ## rare
-fern.data.rare <- read.csv("../data/data_rare_for_modeling.csv", as.is=TRUE)
+fern.data.rare <- read.csv("../data/data_rare_for_modeling.csv", as.is = TRUE)
 
 ## all
-fern.data <- read.csv("../data/data_for_modeling.csv", as.is=TRUE)
-
-head(fern.data.rare)
+fern.data <- read.csv("../data/data_for_modeling.csv", as.is = TRUE)
 
 #######################
 # PART 3: modeling ####
 #######################
-## Building models corresponding to general hypothesis 
+## Building models corresponding to general hypothesis
 
 ### Fitting models only for abundant species
 head(fern.data.ab)
@@ -49,42 +47,48 @@ head(fern.data.rare)
 
 ## Ecological Strategy defined by all the three traits: laminar thickness, life form and indumentum interacting with altitude, drift among species sharing the same ES, local and regional limited dispersal
 m.full.ab <- glmer(abundance ~ thickness*grad + thickness*I(grad^2)
-                ##+ indumentum*grad + indumentum*I(grad^2)
-                +  life_form*grad + life_form*I(grad^2)
-                + (1|spp) + (1|spp:region) + (1|spp:site) + (1+grad|site),
-                data=fern.data.ab, family="poisson",
-                control=glmerControl(optimizer="bobyqa", optCtrl=list(maxfun=5e6)))
+                   ##+ indumentum*grad + indumentum*I(grad^2)
+                   +  life_form*grad + life_form*I(grad^2)
+                   + (1|spp) + (1|spp:region) + (1|spp:site) + (1 + grad|site),
+                   data = fern.data.ab, family = "poisson",
+                   control = glmerControl(optimizer = "bobyqa",
+                                          optCtrl = list(maxfun = 5e6)))
 
 m.full2.ab <- glmer(abundance ~ grad + I(grad^2)
-                 ##+ (1|spp)
-                 + (1|spp:region) + (1|spp:site) + (1+grad|spp) + (1+grad|site),
-                  data=fern.data.ab, family="poisson",
-                control=glmerControl(optimizer="bobyqa", optCtrl=list(maxfun=1e6)))
+                    ##+ (1|spp)
+                    + (1|spp:region) + (1|spp:site) + (1 + grad|spp) + (1 + grad|site),
+                    data = fern.data.ab, family = "poisson",
+                    control = glmerControl(optimizer = "bobyqa",
+                                           optCtrl = list(maxfun = 1e6)))
 
 # ### using estimated parameters to buit a new model
 ss <- getME(m.full2.ab, c("theta","fixef"))
-m.full2b.ab <- update(m.full2.ab, start=ss)
+m.full2b.ab <- update(m.full2.ab, start = ss)
 
 m.neutral.ab <- glmer(abundance ~ (1|spp:region) + (1|spp:site) + (1|site),
-                      data=fern.data.ab, family="poisson",
-                      control=glmerControl(optimizer="bobyqa", optCtrl=list(maxfun=1e6)))
+                      data = fern.data.ab, family = "poisson",
+                      control = glmerControl(optimizer = "bobyqa",
+                                             optCtrl = list(maxfun = 1e6)))
 
 m.niche.ab <- glmer(abundance ~ thickness*grad + thickness*I(grad^2)
                     ##+ indumentum*grad + indumentum*I(grad^2)
                     +  life_form*grad + life_form*I(grad^2)
-                    + (1|spp) + (1+grad|site),
-                    data=fern.data.ab, family="poisson",
-                    control=glmerControl(optimizer="bobyqa", optCtrl=list(maxfun=5e6)))
+                    + (1|spp) + (1 + grad|site),
+                    data = fern.data.ab, family = "poisson",
+                    control = glmerControl(optimizer = "bobyqa",
+                                           optCtrl = list(maxfun = 5e6)))
 
 m.env.ab <- glmer(abundance ~ grad + I(grad^2)
                   + (1+grad|site) + (1+grad|spp),
-                  data=fern.data.ab, family="poisson",
-                  control=glmerControl(optimizer="bobyqa", optCtrl=list(maxfun=5e6)))
+                  data = fern.data.ab, family = "poisson",
+                  control = glmerControl(optimizer = "bobyqa",
+                                         optCtrl = list(maxfun = 5e6)))
 
-m.null.ab <- glmer(abundance ~ 1 
+m.null.ab <- glmer(abundance ~ 1
                    + (1|spp) + (1|region) + (1|site),
-                   data=fern.data.ab, family="poisson",
-                   control=glmerControl(optimizer="bobyqa", optCtrl=list(maxfun=5e6)))
+                   data = fern.data.ab, family = "poisson",
+                   control = glmerControl(optimizer = "bobyqa",
+                                          optCtrl = list(maxfun = 5e6)))
 
 ## Model selection
 m.list <- list(m.full.ab, m.neutral.ab, m.niche.ab, m.null.ab, m.full2b.ab,
@@ -93,7 +97,7 @@ mod.names <- c("niche & neutral", "neutral", "niche", "null", "env & neutral",
                "env")#, "thickness&neutral", "lifef&neutral", "indument&neutral")
 
 ### AIC
-aic.ab <- AICctab(m.list, mnames=mod.names, base=TRUE, weights=TRUE, logLik=TRUE)
+aic.ab <- AICctab(m.list, mnames = mod.names, base = TRUE, weights = TRUE, logLik = TRUE)
 class(aic.ab) <- 'data.frame'
 
 write.table(aic.ab, "../results/aic_table_ab.csv",
@@ -103,46 +107,53 @@ write.table(aic.ab, "../results/aic_table_ab.csv",
 
 ## Ecological Strategy defined by all the three traits: laminar thickness, life form and indumentum interacting with altitude, drift among spp sharing the same ES, local and regional limited dispersal
 m.full.rar <- glmer(abundance ~ thickness*grad + thickness*I(grad^2)
-                ##+ indumentum*grad + indumentum*I(grad^2)
-                +  life_form*grad + life_form*I(grad^2)
-                + (1|spp) + (1|spp:region) + (1|spp:site) + (1+grad|site),
-                data=fern.data.rare, family="poisson",
-                control=glmerControl(optimizer="bobyqa", optCtrl=list(maxfun=5e6)))
+                    ##+ indumentum*grad + indumentum*I(grad^2)
+                    +  life_form*grad + life_form*I(grad^2)
+                    + (1|spp) + (1|spp:region) + (1|spp:site) + (1 + grad|site),
+                    data = fern.data.rare, family = "poisson",
+                    control = glmerControl(optimizer = "bobyqa",
+                                           optCtrl = list(maxfun = 5e6)))
 
 ## Trying different combinations of traits
 m.full2.rar <- glmer(abundance ~ grad + I(grad^2)
                      ##+ (1|spp)
-                     + (1|spp:region) + (1|spp:site) + (1+grad|spp) + (1+grad|site),
-                     data=fern.data.rare, family="poisson",
-                     control=glmerControl(optimizer="bobyqa", optCtrl=list(maxfun=5e6)))
+                     + (1|spp:region) + (1|spp:site) + (1+grad|spp) + (1 + grad|site),
+                     data = fern.data.rare, family = "poisson",
+                     control = glmerControl(optimizer = "bobyqa",
+                                            optCtrl = list(maxfun = 5e6)))
 
 m.neutral.rar <- glmer(abundance ~ (1|spp:region) + (1|spp:site) + (1|site),
-                       data=fern.data.rare, family="poisson",
-                       control=glmerControl(optimizer="bobyqa", optCtrl=list(maxfun=1e6)))
+                       data = fern.data.rare, family = "poisson",
+                       control = glmerControl(optimizer = "bobyqa",
+                                              optCtrl = list(maxfun = 1e6)))
 
 m.niche.rar <- glmer(abundance ~ thickness*grad + thickness*I(grad^2)
                      ##+ indumentum*grad + indumentum*I(grad^2)
                      +  life_form*grad + life_form*I(grad^2)
-                     + (1|spp) + (1+grad|site),
-                     data=fern.data.rare, family="poisson",
-                     control=glmerControl(optimizer="bobyqa", optCtrl=list(maxfun=5e6)))
+                     + (1|spp) + (1 + grad|site),
+                     data = fern.data.rare, family = "poisson",
+                     control = glmerControl(optimizer = "bobyqa",
+                                            optCtrl = list(maxfun = 5e6)))
 
 m.env.rar <- glmer(abundance ~ grad + I(grad^2)
-                   + (1+grad|site) + (1+grad|spp),
-                   data=fern.data.rare, family="poisson",
-                   control=glmerControl(optimizer="bobyqa", optCtrl=list(maxfun=5e6)))
+                   + (1 + grad|site) + (1 + grad|spp),
+                   data = fern.data.rare, family = "poisson",
+                   control = glmerControl(optimizer = "bobyqa",
+                                          optCtrl = list(maxfun = 5e6)))
 
-m.null.rar <- glmer(abundance ~ 1 
+m.null.rar <- glmer(abundance ~ 1
                     + (1|spp) + (1|region) + (1|site),
-                    data=fern.data.rare, family="poisson",
-                    control=glmerControl(optimizer="bobyqa", optCtrl=list(maxfun=5e6)))
+                    data = fern.data.rare, family = "poisson",
+                    control = glmerControl(optimizer = "bobyqa",
+                                           optCtrl = list(maxfun = 5e6)))
 
 ## Model selection
 m.list.rar <- list(m.full.rar, m.neutral.rar, m.niche.rar, m.null.rar, m.full2.rar,
                m.env.rar)#, m.full.thickness, m.full.lifeform, m.full.indument)
 
 ### AIC
-aic.rar <- AICctab(m.list.rar, mnames=mod.names, base=TRUE, weights=TRUE, logLik=TRUE)
+aic.rar <- AICctab(m.list.rar, mnames = mod.names, base = TRUE, weights = TRUE,
+                   logLik = TRUE)
 class(aic.rar) <- 'data.frame'
 
 write.table(aic.rar, "../results/aic_table_rar.csv",
@@ -153,7 +164,7 @@ write.table(aic.rar, "../results/aic_table_rar.csv",
 ## Part 4 : calculating R2 ###################
 ##############################################
 
-### using functions from tutorial in file functions.R 
+### using functions from tutorial in file functions.R
 r2.ab <- r2.full(m.full.ab)
 r2.rar <- r2.neutral(m.neutral.rar)
 
@@ -168,8 +179,8 @@ r2.df$group <- c(rep("abundant", 7), rep("rare", 6))
 r2.df <- r2.df[!r2.df$type %in% c("all", "all.random"),]
 
 #pdf("../figures/barplot.pdf")
-ggplot(r2.df, aes(fill=type, y=R2, x=group)) + 
-    geom_bar(stat="identity", position="fill") +
+ggplot(r2.df, aes(fill = type, y = R2, x = group)) +
+    geom_bar(stat = "identity", position = "fill") +
     theme_classic()
 #dev.off()
 
@@ -178,38 +189,41 @@ ggplot(r2.df, aes(fill=type, y=R2, x=group)) +
 ##############################################
 
 ## Predicted for abundant species by the selected model
-pred.ab <- merTools::predictInterval(m.full.ab,  which="all", type="linear.prediction", level=0.95)
+pred.ab <- merTools::predictInterval(m.full.ab,  which = "all",
+                                     type = "linear.prediction", level = 0.95)
 ## Predicted for rare species
-pred.rare <- merTools::predictInterval(m.neutral.rar,  which="all", type="linear.prediction", level=0.95)
+pred.rare <- merTools::predictInterval(m.neutral.rar,  which = "all",
+                                       type = "linear.prediction", level = 0.95)
 
 ## Mean expected abundances in function of altitude for all species by the combination of the best models
-all.preds <- rbind(cbind(fern.data.ab,combined=pred.ab[pred.ab$effect=="combined","fit"],
-                         fixed=pred.ab[pred.ab$effect=="fixed","fit"]),
-                   cbind(fern.data.rare, combined=pred.rare[pred.rare$effect=="combined","fit"],
-                         fixed=pred.rare[pred.rare$effect=="fixed","fit"]))
+all.preds <- rbind(cbind(fern.data.ab,combined = pred.ab[pred.ab$effect == "combined", "fit"],
+                         fixed = pred.ab[pred.ab$effect == "fixed", "fit"]),
+                   cbind(fern.data.rare, combined = pred.rare[pred.rare$effect == "combined","fit"],
+                         fixed = pred.rare[pred.rare$effect == "fixed","fit"]))
 
 ## Log of mean of observed values, log of sds of predicted by total and random effects + intercepts
 sads.meta <- all.preds %>%
     mutate(random = combined - fixed +
-               ifelse(ab.rare=="rare", fixef(m.neutral.rar)[1] , fixef(m.full.ab)[1]),
-           ab.class=factor(ifelse(ab.rare=="abundant", "Core", "Occasional"))) %>%
+               ifelse(ab.rare == "rare", fixef(m.neutral.rar)[1] , fixef(m.full.ab)[1]),
+           ab.class=factor(ifelse(ab.rare == "abundant", "Core", "Occasional"))) %>%
     group_by(spp, ab.class) %>%
-    summarise(mean.obs=mean(abundance), sd.obs=sd(abundance),
-              lwr.obs=ifelse(mean.obs-sd.obs>0,mean.obs-sd.obs,1/30), upr.obs=mean.obs+sd.obs,
-              mean.comb=mean(exp(combined)), sd.comb=sd(exp(combined)),
-              mean.random=mean(exp(random)), sd.random=sd(exp(random)),
-              lwr.random=ifelse(mean.obs-sd.random>0,mean.obs-sd.random,1/30), upr.random=mean.obs+sd.random
-              )%>%
+    summarise(mean.obs = mean(abundance), sd.obs = sd(abundance),
+              lwr.obs = ifelse(mean.obs - sd.obs > 0, mean.obs - sd.obs, 1/30),
+              upr.obs = mean.obs + sd.obs,
+              mean.comb = mean(exp(combined)), sd.comb = sd(exp(combined)),
+              mean.random = mean(exp(random)), sd.random = sd(exp(random)),
+              lwr.random = ifelse(mean.obs - sd.random > 0, mean.obs - sd.random, 1 / 30),
+              upr.random = mean.obs+sd.random) %>%
     ungroup() %>%
-    mutate(sp.rank=rank(-mean.obs, ties.method="first"),
-           sp.rank.c=rank(-mean.comb, ties.method="first"),
-           sp.rank.r=rank(-mean.random, ties.method="first"))
+    mutate(sp.rank = rank(-mean.obs, ties.method = "first"),
+           sp.rank.c = rank(-mean.comb, ties.method = "first"),
+           sp.rank.r = rank(-mean.random, ties.method = "first"))
 
 
 ## writing predicted values
 write.table(all.preds, '../results/predicted.csv',
-            col.names=TRUE, row.names=FALSE, sep=",")
+            col.names = TRUE, row.names = FALSE, sep = ",")
 
 
 write.table(sads.meta, '../results/sads_metacommunity.csv',
-            col.names=TRUE, row.names=FALSE, sep=",")
+            col.names = TRUE, row.names = FALSE, sep = ",")
