36b68d8d6fe68f86b22ab13c6f75864ff3709538
saramortara <saramortara@gmail.com>
2020-08-23 22:28:17 -0300

    summarizing results from simulations

--- code/04_graphics_supplementary.R
+++ code/04_graphics_supplementary.R
@@ -1,34 +1,46 @@
+# Libraries
 library(ggplot2)
 library(dplyr)
+
 #library(colortools)
-
-# First set of plots: one round of simulations #################################
-t1 <- read.csv("results/test1_tutorial.csv")
-t2 <- read.csv("results/test2_tutorial.csv")
-
-all.t1 <- tapply(t1$Value,
-                 list(t1$Community),
-                 sum, na.rm = TRUE)
-
-all.t2 <- tapply(t2$Value,
-                 list(t2$Community),
-                 sum, na.rm = TRUE)
-
-table(t1$Community)
-table(t2$Community)
-
-t1$Total <- rep(all.t1, each = 6)
-t2$Total <- rep(all.t2, each = 6)
-
-t1$Proportion <- t1$Value/t1$Total
-t2$Proportion <- t2$Value/t2$Total
-
+# Setting color palette
 neutral <- "#091346"
 niche <- "#801411"
 nineu <- "#D6A838" #"#e1b727"
 idiosyncratic <- "#B4B1AC"
 
 paleta <- c(niche, neutral, nineu, grey)
+
+# Reading output from simulations
+## Listing files
+prop_files <- list.files("results/simulations", pattern = ".*_prop.csv$", full.names = TRUE)
+effects_files <-  list.files("results/simulations", pattern = ".*_effects.csv$", full.names = TRUE)
+
+# Proportion table --------------------------------------------------------
+## Setting names
+comm <- c("Deterministic with right traits", "Deterministic with wrong traits", "Stochastic with right traits")
+models <- c("Niche-neutral", "Niche-neutral (no traits)", "Niche", "Niche (no traits)", "Neutral", "Idyosyncratic")
+
+# Proportions of the best fitted models
+prop_list <- lapply(prop_files, read.csv)
+prop_df <- t(bind_cols(lapply(prop_list, function(x) x[, 2])))
+colnames(prop_df) <- models
+rownames(prop_df) <- comm
+
+
+# Effects -----------------------------------------------------------------
+
+effects_list <- lapply(effects_files, read.csv)
+names(effects_list) <- comm
+effects_df <- bind_rows(effects_list, .id = "scenario") %>%
+  rename(stat = X)
+effects_df$stat[effects_df$stat == ""] <- "mean"
+
+effect_mean <- filter(effects_df, stat == "mean")
+effect_lwr <- filter(effects_df, stat == "2.5%")
+effect_lwr <- filter(effects_df, stat == "97.5%")
+
+
 
 # other stuff w/ colors
 # library(colortools)
@@ -38,31 +50,31 @@ paleta <- c(niche, neutral, nineu, grey)
 # wheel(neutral, num = 10)
 # wheel(nineu, num = 10)
 
-p1 <- ggplot(t1, aes(x = Community,
-                     y = Proportion,
-                     fill = Effect)) +
-  geom_bar(stat = "identity", color = "black") +
-  theme_classic()
-
-
-p1
-p2 <- ggplot(t2, aes(x = Community,
-                     y = Proportion,
-                     fill = Effect)) +
-  geom_bar(stat = "identity", color = "black") +
-  theme_classic()
-
-p2
-
-grid.arrange(p1, p2, nrow = 1)
-
-png("figures/test1.png")
-p1
-dev.off()
-
-png("figures/test2.png")
-p2
-dev.off()
-
-p1
-p2
+# p1 <- ggplot(t1, aes(x = Community,
+#                      y = Proportion,
+#                      fill = Effect)) +
+#   geom_bar(stat = "identity", color = "black") +
+#   theme_classic()
+#
+#
+# p1
+# p2 <- ggplot(t2, aes(x = Community,
+#                      y = Proportion,
+#                      fill = Effect)) +
+#   geom_bar(stat = "identity", color = "black") +
+#   theme_classic()
+#
+# p2
+#
+# grid.arrange(p1, p2, nrow = 1)
+#
+# png("figures/test1.png")
+# p1
+# dev.off()
+#
+# png("figures/test2.png")
+# p2
+# dev.off()
+#
+# p1
+# p2
