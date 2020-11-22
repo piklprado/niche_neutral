080aaa5453e1f21a9fff2c8d222272ba99b30175
saramortara <saramortara@gmail.com>
2020-11-22 20:38:08 -0300

    adding new graphic w/ six example species

--- code/03_graphics.R
+++ code/03_graphics.R
@@ -10,9 +10,16 @@
 library(dplyr)
 library(ggplot2)
 library(gridExtra)
+library(ggpubr)
 
 ## functions to make graphics
 source("code/functions.R")
+
+## reading data
+all.preds <- read.csv("results/predicted.csv")
+sads.meta <- read.csv("results/sads_metacommunity.csv")
+fern.data <- read.csv("data/data_for_modeling.csv")
+spp <- read.csv("data/species_list.csv")
 
 # Colors from manuscript
 # niche <- rgb(202, 0, 32, maxColorValue = 255)
@@ -33,14 +40,7 @@ paleta <- c(niche, neutral, nineu, grey)
 paleta2 <- c(GR, MR, PR)
 
 # plot(1:4, col = paleta, pch = 19, cex = 5)
-# plot(1:4, col = c(nineu, paleta2), pch = 19, cex = 5)
-
-## reading data
-all.preds <- read.csv("results/predicted.csv")
-sads.meta <- read.csv("results/sads_metacommunity.csv")
-fern.data <- read.csv("data/data_for_modeling.csv")
-
-head(sads.meta)
+plot(1:3, col = paleta2, pch = 19, cex = 5)
 
 ### The figure ###
 ## 1st panel of the figure: RAD with total and random part of standard deviations
@@ -58,7 +58,8 @@ fig.meta1
 
 ## 2nd panel: Abundances of selected species over the Gradient
 ## Most abundant of core species
-fig.meta2 <- fern.data[fern.data$spp == sads.meta$spp[sads.meta$sp.rank == 1], ] %>%
+fig.meta2 <- fern.data %>%
+    filter(spp == "sp120") %>% # rank 1
     ggplot(aes(altitude, abundance)) +
     stat_smooth(se = FALSE, aes(color = region), alpha = 0.5, span = 0.6) +
     geom_jitter(aes(color = region), size = 2.5, alpha = 0.5) +
@@ -67,15 +68,110 @@ fig.meta2 <- fern.data[fern.data$spp == sads.meta$spp[sads.meta$sp.rank == 1], ]
                             x = "", y = "Abundance", color = "Region", tag = "B") +
     theme_classic()
 
-fig.meta2
+#fig.meta2
 
 ## 3rd panel: Most abundant of occasional species
-fig.meta3 <- fig.meta2 %+% fern.data[fern.data$spp == sads.meta$spp[sads.meta$sp.rank == 41],] +
+fig.meta3 <- fig.meta2 %+% filter(fern.data, spp == "sp159") +
     labs(title = expression(bold("Occasional:") ~ italic("Trichomanes polypodioides")),
-                            x = "", y = "", color = "Region", tag = "C") +
+         x = "", y = "", color = "Region", tag = "D") +
     theme_classic()
 
-fig.meta3
+#fig.meta3
+
+# New proposal, selection of six species to show differences among life forms
+# selected species (ep, hemi, ter)
+# Occasional: sp159, sp033, sp028
+# Rare: sp084, sp120, sp012
+# Core ep
+
+sp.sel <- c("sp158", "sp033", "sp028",
+            "sp084", "sp120", "sp030")
+
+
+names.sp <- spp %>%
+    filter(species %in% sp.sel) %>%
+    select(name_accepted, species) %>%
+    rename(spp = species)
+
+fig.sp <- list()
+
+data.sel <- fern.data %>%
+    filter(spp %in% sp.sel) %>%
+    left_join(names.sp, by = "spp") %>%
+    mutate(spp = factor(.$spp,
+                        levels = c("sp084", "sp120", "sp030",
+                                   "sp158", "sp033", "sp028"))) %>%
+    arrange(spp)
+
+
+# fig.sp <- data.sel %>%
+#     ggplot(aes(altitude, abundance)) +
+#     stat_smooth(se = FALSE, aes(color = region), alpha = 0.5, span = 0.6) +
+#     geom_jitter(aes(color = region), size = 2.5, alpha = 0.5) +
+#     scale_color_manual(values = paleta2) +
+#     facet_wrap(~ name_accepted, scales = "free",
+#                nrow = 3, ncol = 2) +
+#     labs(#title = expression(bold("Core:") ~ italic("Polybotrya cylindrica")),
+#         x = "Altitude (m)", y = "Abundance", color = "Region") +
+#     theme_classic() +
+#     theme(strip.text = element_text(hjust = 0, face = "italic"),
+#           strip.background = element_blank(),
+#           legend.position = "bottom")
+
+fig.sp <- list()
+
+for (i in 1:length(sp.sel)) {
+    fig.sp[[i]] <- ggplot(data = filter(data.sel, spp == unique(data.sel$spp)[i]),
+                          aes(altitude, abundance)) +
+        stat_smooth(se = FALSE, aes(color = region), alpha = 0.5, span = 0.6) +
+        geom_jitter(aes(color = region), size = 2.5, alpha = 0.5) +
+        scale_color_manual(values = paleta2) +
+        labs(title = "",
+             subtitle = unique(data.sel$name_accepted)[i],
+             x = "", y = "", color = "Region",
+             tag = LETTERS[i]) +
+        theme_classic() +
+        theme(plot.subtitle = element_text(face = "italic"))
+}
+
+
+# writing text around plots
+fig1 <- annotate_figure(fig.sp[[1]] + theme(legend.position = "none"),
+                        right = text_grob(bquote(""), rot = -90))
+fig2 <- annotate_figure(fig.sp[[2]] + theme(legend.position = "none"),
+                        right = text_grob(bquote(""), rot = -90))
+fig3 <- annotate_figure(fig.sp[[3]] + theme(legend.position = "none"),
+                        right = text_grob(bquote(bold("Core")), rot = -90))
+fig6 <- annotate_figure(fig.sp[[6]] + theme(legend.position = "none"),
+                        right = text_grob(bquote(bold("Occasional")), rot = -90))
+# fig6 <- annotate_figure(fig.sp[[6]] + theme(legend.position = "none"),
+#                         right = text_grob(bquote(bold("Terrestrial")), rot = -90))
+
+# combining species figures
+fig.sp.combined <- ggarrange(fig1 +
+                                 ggtitle(expression(bold("Epiphyte"))) +
+                                 theme(plot.title = element_text(hjust = 0.5, size = 12)),
+                             fig2 +
+                                 ggtitle(expression(bold("Hemiepiphyte"))) +
+                                 theme(plot.title = element_text(hjust = 0.5, size = 12)),
+                             fig3 +
+                                 ggtitle(expression(bold("Terrestrial"))) +
+                                 theme(plot.title = element_text(hjust = 0.5, size = 12)),
+                             fig.sp[[4]],
+                             fig.sp[[5]],
+                             fig6,
+                             ncol = 3, nrow = 2,
+                             common.legend = TRUE, legend = "bottom")
+
+annotate_figure(fig.sp.combined, left = "Abundance", bottom = "Altitude")
+
+# saving combined plot of species
+cairo_pdf("figures/core_ocasional_species.pdf", width = 8.5, height = 7.5)
+
+annotate_figure(fig.sp.combined, left = "Abundance", bottom = "Altitude")
+
+dev.off()
+
 
 ## Arranging all panels in a sigle figure, publication quality (see functions.R)
 ## List of panels
@@ -91,15 +187,19 @@ fig.meta.list <- list(
 ## Arrange with grid.arrange
 cairo_pdf("figures/rad_metacommunity.pdf", width = 9, height = 7.5)
 
-grid.arrange(
-    grobs = fig.meta.list,
-    bottom = textGrob("Altitude (m)", gp = gpar(fontface = "bold", cex = 1.2), vjust = -1.25),
-    layout_matrix = matrix(c(1, 1, 2, 3), ncol = 2, byrow = TRUE)
-)
+fig.meta1 + #scale_colour_Publication() +
+    labs(tag = "") +
+    theme_Publication()
+# grid.arrange(
+#     grobs = fig.meta.list,
+#     bottom = textGrob("Altitude (m)", gp = gpar(fontface = "bold", cex = 1.2), vjust = -1.25),
+#     layout_matrix = matrix(c(1, 1, 2, 3), ncol = 2, byrow = TRUE)
+# )
 
 dev.off()
 
-# Figure R2
+# Figure R2 ####
+# it wont be a figure, just values on text
 
 r2 <- read.csv("results/r2_table.csv")
 
